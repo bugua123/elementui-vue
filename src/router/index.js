e6cc1184cc7b26ef70f9071a9b41503bcdc9ee47
bugua123 <47909592@163.com>
2020-01-15 10:16:39 +0800

    初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Home from '@/components/Home'
+import Home from '../components/Home'
 Vue.use(Router)
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+Vue.prototype.$axios = axios
 
 export default new Router({
   routes: [
